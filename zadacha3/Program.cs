036e94f639b62bd9f031d3196d87c0c39d5bd90a
OlgaT <oliunea-t2002@mail.ru>
2022-08-15 01:30:19 +0300

    add zadacha 3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними
+// в 2D простарнстве. Например: А(3,6); В(2,1) - 5,09. А(7,-5); В(1,-1) - 7,21.
+Console.WriteLine("Введите координату Х1");
+var numberX1 = Console.ReadLine();
+int x1 = int.Parse(numberX1!);
+
+Console.WriteLine("Введите координату Y1");
+var numberY1 = Console.ReadLine();
+int y1 = int.Parse(numberY1!);
+
+Console.WriteLine("Введите координату Х2");
+var numberX2 = Console.ReadLine();
+int x2 = int.Parse(numberX2!);
+
+Console.WriteLine("Введите координату Y1");
+var numberY2 = Console.ReadLine();
+int y2 = int.Parse(numberY2!);
+
+double AB = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+Console.WriteLine("\n" + AB.ToString("N2"));
